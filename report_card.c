993a25d9a43fa9bb27d9f6a02da616cd63444872
JeongYoHan <wsxc1226@naver.com>
2020-11-28 00:28:39 +0900

    add math

--- report_card.c
+++ report_card.c
@@ -4,10 +4,11 @@ int main() {
 	printf("This program print report card.\n");
 
 	int kor = 90;
+	int math = 80;
 	int sum = 90;
 
 	printf("Korean : %d\n", kor);
-
+	printf("Math : %d\n", math);
 	printf("Sum : %d\n",sum);
 
 	return 0;
