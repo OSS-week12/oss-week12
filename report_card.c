95f6a6a6c574200088e4c574ba7c22e5b315db37
slslslrhfem <57289103+slslslrhfem@users.noreply.github.com>
2020-11-29 05:17:09 +0900

    Update report_card.c

--- report_card.c
+++ report_card.c
@@ -5,13 +5,16 @@ int main() {
 
 	int kor = 90;
 	int math = 80;
-	int sum = 90;
 	int eng = 80;
+	int sum = (kor + math + eng);
+	int avg = sum/3;
+
 
 	printf("Korean : %d\n", kor);
 	printf("Math : %d\n", math);
 	printf("English : %d\n", eng);
 	printf("Sum : %d\n",sum);
+	printf("Average : %d\n",avg);
 
 	return 0;
 }
