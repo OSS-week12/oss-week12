81674645651a15ddb4419470619f654cd961c8d0
TaewanKIM20 <fnzk2019@g.skku.edu>
2020-11-28 20:07:27 +0900

    Add English

--- report_card.c
+++ report_card.c
@@ -6,9 +6,11 @@ int main() {
 	int kor = 90;
 	int math = 80;
 	int sum = 90;
+	int eng = 80;
 
 	printf("Korean : %d\n", kor);
 	printf("Math : %d\n", math);
+	printf("English : %d\n", eng);
 	printf("Sum : %d\n",sum);
 
 	return 0;
